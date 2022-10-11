9bcf17dda5395d2534ea26114babefcd428f4d4d
Sebastien Aubert <sebastien.aubert@hotmail.ch>
2022-10-11 18:46:23 +0200

    resolve warning message

--- App.js
+++ App.js
@@ -1,10 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button, Image } from "react-native";
+import { useState } from "react";
 
 export default function App() {
+  const [catURL, setCatURL] = useState(" ");
+
+  const getCat = () => {
+    fetch("https://aws.random.cat/meow")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setCatURL(data.file);
+      });
+  };
+  //getCat();
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>My first app with React Native</Text>
+
+      <Button
+        onPress={getCat}
+        title="Show me a cat"
+        color="#841584"
+        accessibilityLabel="Learn more about this purple button"
+      />
+
+      <Image
+        style={styles.tinyLogo}
+        source={{
+          uri: catURL,
+        }}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +40,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  tinyLogo: {
+    width: 350,
+    height: 350,
   },
 });
